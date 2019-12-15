6b522725fafdeb4d93b761d82f44bbbf6d20b03f
Anthony <8753187+bigfoott@users.noreply.github.com>
2019-12-14 20:12:50 -0500

    init

---
+++ vrsr.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<link rel="icon" href="images/vrsrfavicon.png">
+		
+		<title>VR Speedrunning</title>
+		
+		<meta content="Bigft.io" property="og:site_name">
+		<meta content="VR SPeedrunning" property="og:title">
+		<meta content="#4cd1ff" name="theme-color">
+		
+		<script src="https://kit.fontawesome.com/d16c543bf8.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+		<link rel="stylesheet" href="assets/css/vrsr.css"> 
+	</head>
+	<body>
+		<section class="section">
+			<div class="container">
+			</div>
+		</section>
+	</body>
+</html>
